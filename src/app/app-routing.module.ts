afd551e8ed6685c133990f496ed84a9e2938042e
6ssou <6ssou@protonmail.com>
2022-06-19 00:12:52 +0200

    separate todos inside module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
-import { TodosComponent } from './todos/todos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: "", loadChildren: () => import("./posts/posts.module").then(m => m.PostsModule)},
-  {path: "todos", component: TodosComponent}
+  { path: "", loadChildren: () => import("./posts/posts.module").then(m => m.PostsModule) },
+  { path: "todos", loadChildren: () => import("./todos/todos.module").then(m => m.TodosModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import { TodosComponent } from './todos/todos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
